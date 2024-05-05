9de4fbf4fe9e4904e50e30f0cebb170f13940480
Alberto Masogo Mendoza <e-atmg@gft.com>
2024-05-05 14:35:21 +0200

    [structure] chore(*): add console

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -5,7 +5,7 @@ async function sendTestEmail(req, res) {
 
     try {
         await sendEmail(to, subject, text);
-        res.status(200).json({ message: 'Correo electrónico enviado correctamente' });
+        res.status(200).json({ message: `Correo electrónico enviado correctamente  ${to}`});
     } catch (error) {
         res.status(500).json({ error: 'Error al enviar el correo electrónico' });
     }
