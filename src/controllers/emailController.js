639fe2cdba10da151c21158b5d26938935423e6f
Alberto Masogo Mendoza <e-atmg@gft.com>
2024-05-05 14:33:18 +0200

    [structure] chore(*): fix sendgrid server in production

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -1,10 +1,8 @@
 const { sendEmail } = require('../utils/emailSender');
 
 async function sendTestEmail(req, res) {
-    //const { to, subject, text } = req.body;
-    const to = 'masugusdev@proton.me';
-    const subject = 'Página de boda';
-    const text = 'TEST DE CORREO';
+    const { to, subject, text } = req.body;
+
     try {
         await sendEmail(to, subject, text);
         res.status(200).json({ message: 'Correo electrónico enviado correctamente' });
