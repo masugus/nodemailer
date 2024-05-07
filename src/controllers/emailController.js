b4df401799b8b4797b4af8665ca357c12e808077
Alberto Masogo Mendoza <e-atmg@gft.com>
2024-05-07 02:00:08 +0200

    [structure] chore(*): fix body petition

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -4,9 +4,18 @@ const config = require("../config");
 
 async function sendTestEmail(req, res) {
   console.log("body del request", req.body);
+
   const subject = "Respuesta al Formulario de la boda";
   const to = config.to;
-  const html = generateEmailTemplate(req.body);
+  let body;
+  try {
+    // Intenta parsear el body a JSON
+    body = JSON.parse(req.body);
+  } catch (error) {
+    // Si el parse falla, usa el body original
+    body = req.body;
+  }
+  const html = generateEmailTemplate(body);
 console.log('html', html);
   try {
     await sendEmail(to, subject, html);
