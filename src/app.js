e294cf5551307b0ad6734c3c3fa6f0c152b49f90
Alberto Masogo Mendoza <e-atmg@gft.com>
2024-05-07 22:18:56 +0200

    [structure] chore(*): enable cors

--- src/app.js
+++ src/app.js
@@ -23,8 +23,8 @@ const corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-// app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
+// app.use(cors());
 
 // Middleware
 app.use(bodyParser.json());
